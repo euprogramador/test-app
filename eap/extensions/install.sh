f516bba19533f71407c932bafaa3120ebfcee8cf
Administrator <admin@example.com>
2023-09-13 01:53:15 -0300

    teste de adição de script customização

--- eap/extensions/install.sh
+++ eap/extensions/install.sh
@@ -4,6 +4,7 @@ source /usr/local/s2i/install-common.sh
 injected_dir=$1
 echo "Running on injected_dir=${injected_dir}"
 
+install_modules ${injected_dir}/modules
 
 $JBOSS_HOME/bin/jboss-cli.sh --file="${injected_dir}/adapter-elytron-install-offline.cli"
 
